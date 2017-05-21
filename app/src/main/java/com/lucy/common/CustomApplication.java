0ed0df3a4a1e17d9362664349740d306faf5c9a9
908305918 <908305918@qq.com>
2017-05-21 14:04:58 +0800

    rxbus

--- app/src/main/java/com/lucy/common/CustomApplication.java
+++ app/src/main/java/com/lucy/common/CustomApplication.java
@@ -4,8 +4,10 @@ import android.app.Application;
 
 import com.facebook.stetho.Stetho;
 import com.facebook.stetho.okhttp3.StethoInterceptor;
+import com.threshold.rxbus2.RxBus;
 import com.zhy.http.okhttp.OkHttpUtils;
 
+import io.reactivex.android.schedulers.AndroidSchedulers;
 import okhttp3.OkHttpClient;
 
 /**
@@ -16,7 +18,11 @@ public class CustomApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+
+        RxBus.config(AndroidSchedulers.mainThread());
+
         Stetho.initializeWithDefaults(this);
+
         OkHttpClient okHttpClient = new OkHttpClient.Builder()
                 .addNetworkInterceptor(new StethoInterceptor())
                 .build();
