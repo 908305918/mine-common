3a204f69173c6429370dbff753e7c40fa968508d
908305918 <908305918@qq.com>
2017-05-21 13:50:12 +0800

    rxbus

---
+++ app/src/main/java/com/lucy/common/activity/BaseActivity.java
@@ -0,0 +1,16 @@
+package com.lucy.common.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by YJB on 2017/5/21.
+ */
+
+public class BaseActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
