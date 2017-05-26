00916d7b57eb34c0c577bd425288df6531d53d77
908305918 <908305918@qq.com>
2017-05-26 22:30:26 +0800

    rxjava

--- app/src/main/java/com/lucy/common/activity/StateButtonActivity.java
+++ app/src/main/java/com/lucy/common/activity/StateButtonActivity.java
@@ -6,12 +6,21 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.jakewharton.rxbinding2.view.RxView;
 import com.lucy.common.R;
 import com.lucy.common.view.StateButton;
 import com.zhy.http.okhttp.OkHttpUtils;
 import com.zhy.http.okhttp.callback.StringCallback;
 
+import java.util.concurrent.Callable;
+
+import io.reactivex.Notification;
+import io.reactivex.Observable;
+import io.reactivex.ObservableSource;
+import io.reactivex.functions.Consumer;
+import io.reactivex.functions.Function;
 import okhttp3.Call;
+import okhttp3.Response;
 
 /**
  * Created by Administrator on 2016/11/15.
@@ -25,20 +34,30 @@ public class StateButtonActivity extends Activity {
 
         final StateButton button = (StateButton) findViewById(R.id.state_button);
         final TextView textView = (TextView) findViewById(R.id.tv_content);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                OkHttpUtils.get().url("http://gc.ditu.aliyun.com/geocoding?a=合肥市").build().execute(new StringCallback() {
-                    @Override
-                    public void onError(Call call, Exception e, int id) {
-                        textView.setText("请求失败");
-                    }
 
-                    @Override
-                    public void onResponse(String response, int id) {
-                        textView.setText(response);
-                    }
-                });
+        RxView.clicks(button).flatMap(new Function<Object, ObservableSource<Response>>() {
+            @Override
+            public ObservableSource<Response> apply(Object o) throws Exception {
+                return loadData();
+            }
+        }).materialize().subscribe(new Consumer<Notification<Response>>() {
+            @Override
+            public void accept(Notification<Response> o) throws Exception {
+                if (o.isOnNext()) {
+                    textView.setText(o.getValue().body().string());
+                } else if (o.isOnError()) {
+                    textView.setText(o.getError().toString());
+                }
+            }
+        });
+    }
+
+    private Observable<Response> loadData() {
+        return Observable.fromCallable(new Callable<Response>() {
+            @Override
+            public Response call() throws Exception {
+                String url = "http://gc.ditu.aliyun.com/geocoding?a=合肥市";
+                return OkHttpUtils.get().url(url).build().execute();
             }
         });
     }
