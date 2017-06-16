bd86fc8ba56b8604583f32fb975c9f7fe1bb5018
908305918 <908305918@qq.com>
2017-06-16 15:49:42 +0800

    提交

--- app/src/main/java/com/lucy/common/CustomApplication.java
+++ app/src/main/java/com/lucy/common/CustomApplication.java
@@ -2,8 +2,6 @@ package com.lucy.common;
 
 import android.app.Application;
 
-import com.facebook.stetho.Stetho;
-import com.facebook.stetho.okhttp3.StethoInterceptor;
 import com.zhy.http.okhttp.OkHttpUtils;
 
 import okhttp3.OkHttpClient;
@@ -16,9 +14,7 @@ public class CustomApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        Stetho.initializeWithDefaults(this);
         OkHttpClient okHttpClient = new OkHttpClient.Builder()
-                .addNetworkInterceptor(new StethoInterceptor())
                 .build();
         OkHttpUtils.initClient(okHttpClient);
     }
--- app/src/main/java/com/lucy/common/activity/StateButtonActivity.java
+++ app/src/main/java/com/lucy/common/activity/StateButtonActivity.java
@@ -1,15 +1,20 @@
 package com.lucy.common.activity;
 
 import android.app.Activity;
+import android.app.Dialog;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 
+import com.hss01248.lib.StytledDialog;
 import com.lucy.common.R;
+import com.lucy.common.util.NotificationsUtils;
 import com.lucy.common.view.StateButton;
 import com.zhy.http.okhttp.OkHttpUtils;
 import com.zhy.http.okhttp.callback.StringCallback;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import okhttp3.Call;
 
@@ -17,6 +22,7 @@ import okhttp3.Call;
  * Created by Administrator on 2016/11/15.
  */
 public class StateButtonActivity extends Activity {
+    private TextView mTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,22 +30,46 @@ public class StateButtonActivity extends Activity {
         super.setContentView(R.layout.activity_statebutton);
 
         final StateButton button = (StateButton) findViewById(R.id.state_button);
-        final TextView textView = (TextView) findViewById(R.id.tv_content);
+        final StateButton button1 = (StateButton) findViewById(R.id.state_cancel);
+        mTextView = (TextView) findViewById(R.id.tv_content);
+
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                OkHttpUtils.get().url("http://gc.ditu.aliyun.com/geocoding?a=合肥市").build().execute(new StringCallback() {
-                    @Override
-                    public void onError(Call call, Exception e, int id) {
-                        textView.setText("请求失败");
-                    }
-
-                    @Override
-                    public void onResponse(String response, int id) {
-                        textView.setText(response);
-                    }
-                });
+                //request(true);
+                boolean isOpen = NotificationsUtils.isNotificationEnabled(StateButtonActivity.this);
+                mTextView.setText("通知栏是否开启：" + isOpen);
+            }
+        });
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                request(false);
             }
         });
     }
+
+    private void request(boolean gzip) {
+        final Dialog dialog = StytledDialog.showProgressDialog(this, "正在加载...", false, false);
+        final long startTime = System.currentTimeMillis();
+        Map<String, String> headers = new HashMap<>();
+        if (!gzip) {
+            headers.put("Accept-Encoding", "");
+        }
+        OkHttpUtils.post().url("http://192.168.30.3:9999/").headers(headers).build().execute(new StringCallback() {
+            @Override
+            public void onError(Call call, Exception e, int id) {
+                dialog.dismiss();
+                mTextView.setText(e.toString());
+            }
+
+            @Override
+            public void onResponse(String response, int id) {
+                dialog.dismiss();
+                response = (System.currentTimeMillis() - startTime) + "\n" + response;
+                mTextView.setText(response);
+            }
+        });
+    }
+
 }
