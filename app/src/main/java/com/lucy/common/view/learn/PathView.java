bcf501e9f6893c84f50a397964fbd89267f75a6d
y908305918 <908305918@qq.com>
2020-02-13 13:11:37 +0800

    no message

---
+++ app/src/main/java/com/lucy/common/view/learn/PathView.java
@@ -0,0 +1,40 @@
+package com.lucy.common.view.learn;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class PathView extends View {
+    private Paint mPaint;
+
+    public PathView(Context context) {
+        super(context);
+        init();
+    }
+
+    public PathView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public PathView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    private void init() {
+        mPaint = new Paint();
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        canvas.drawColor(Color.RED);
+        mPaint.setColor(Color.GREEN);
+        canvas.drawCircle(150, 150, 100, mPaint);
+    }
+}
